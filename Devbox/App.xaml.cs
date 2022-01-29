99e4e3d98995d527e5e25ec824e27d341bf0ef9b
Suncoast Software <53012974+suncoast-software@users.noreply.github.com>
2022-01-28 21:12:31 -0500

    Add project files.

---
+++ Devbox/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Devbox
+{
+    public partial class App : Application
+    {
+        private readonly IHost _host;
+
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder().ConfigureServices(s =>
+            {
+
+            }).Build();
+        }
+    }
+}
